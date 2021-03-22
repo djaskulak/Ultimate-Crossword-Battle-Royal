5d8d694062e189d3bdb7004fc2c637f4d395eed7
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-03-22 16:41:05 -0700

    edited header again

--- src/header.js
+++ src/header.js
@@ -1,35 +1,27 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Navbar from 'react-bootstrap/Navbar';
+import NavLink from 'react-bootstrap/NavLink';
 import "./header.css";
 
 function Header() {
   return (
-    <div class="header">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home" id="head"><img src="/CW_logo.png"/></Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className="header">
+      <header>
+        <img src="./CW_logo.png" alt="logo"></img>
+
+        <div>
+        <NavLink
+        className="nav-link"
+        activeClassName="nav-link-active"
+        exact
+        to="/">Home</NavLink>
+
+        <NavLink
+        className="nav-link"
+        activeClassName="nav-link-active"
+        to="/play">Play</NavLink>
+        </div>
+      </header>
+
     </div>
   );
 }
