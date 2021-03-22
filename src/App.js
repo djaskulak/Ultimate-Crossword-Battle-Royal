828759c5ed6a8fd17782acb006ff9a311d31e496
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-03-22 15:11:07 -0700

    navbar
    imported React Bootstrap and started building navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Crossword from './Crossword.js';
+import Header from './header.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <Header />
+      </div>
       <div>
         <Crossword/>
       </div>
