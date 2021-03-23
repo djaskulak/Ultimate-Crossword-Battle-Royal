246352617d73b202bebab6552eb219b94a10a1bf
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-03-23 12:16:31 -0700

    Update header.js

--- src/header.js
+++ src/header.js
@@ -1,42 +1,28 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Nav from 'react-bootstrap/Nav';
-import logo from "./CW_logo.png";
+import NavLink from 'react-bootstrap/NavLink';
+import "./Header.css";
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Navbar.Brand href="#home">
-        <img
-          src={logo}
-          height='60'
-          width='120'
-          className="d-inline-block align-top"
-          alt="React Bootstrap logo"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#rules">Rules</Nav.Link>
-          <NavDropdown title="Play" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Solo</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Battle</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <NavDropdown title="Profile" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Past Games</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Friends</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Settings</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Log Out</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className="Header">
+      <header>
+        <img src="./CW_logo.png" alt="logo"></img>
+
+        <div>
+        <NavLink
+        className="nav-link"
+        activeClassName="nav-link-active"
+        exact
+        to="/">Home</NavLink>
+
+        <NavLink
+        className="nav-link"
+        activeClassName="nav-link-active"
+        to="/play">Play</NavLink>
+        </div>
+      </header>
+
+    </div>
   );
 }
 
